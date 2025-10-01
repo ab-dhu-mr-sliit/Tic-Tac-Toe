52ca0b1fe0a067e6d27a58afebb4dd54617abe2b
Abdur Rahman M.R <it25103195@my.sliit.lk>
2025-10-01 11:08:48 +0530

    Update 0.All Header files.c
    Version 2 Updates

--- 0.All Header files.c
+++ 0.All Header files.c
@@ -2,12 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 #include <windows.h> 
+#include <conio.h>
 
 // 0.All Header files.c
-
-#ifndef GAME_H
-#define GAME_H
-
 
 // Board setup
 char **createBoard(int size);
@@ -26,6 +23,14 @@ int switchPlayer(int currentTurn, int totalPlayers);
 
 // Logging
 void logMove(FILE *fp, char player, int row, int col);
+void logMode(FILE *fp, int mode);
+void logisValidMode(FILE *fp, int mode);
+void logSize(FILE *fp, int size);
+void logisValidSize(FILE *fp, int size);
+void logisValidMove(FILE *fp, int row, int col);
 void logWins(FILE *fp, char symbols);
+void logComputerWin(FILE *fp);
+void logdisplayBoard(FILE *fp, char **board, int size);
+
 
 #endif
