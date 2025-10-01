273fd92b9c5fa9fd263387ce8b29a695f32dd321
Abdur Rahman M.R <it25103195@my.sliit.lk>
2025-10-01 11:12:56 +0530

    Update 4.displayBoard Function.c
    add row, column numbers in display board

--- 4.displayBoard Function.c
+++ 4.displayBoard Function.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 
 void displayBoard(char **board, int size) {
-    printf("\n");
+    puts(" ");
+    printf(" ");
+    for(int i = 0; i < size; i++){
+        printf("   %d", i);
+    }
+    printf("\n\n");
     for (int i = 0; i < size; i++) {
+        printf("%d  ", i);
         // Print each row
         for (int j = 0; j < size; j++) {
             printf(" %c ", board[i][j]);
             if (j < size - 1) printf("|");
         }
         printf("\n");
+        printf("   ");
 
         // Print horizontal separator between rows
         if (i < size - 1) {
