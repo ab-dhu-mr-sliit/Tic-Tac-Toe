2f76d50758a603378bf123f0d348b953add6499e
Abdur Rahman M.R <it25103195@my.sliit.lk>
2025-10-05 14:53:00 +0530

    Update 23.exit game function.c
    now, clear the input buffer

--- 23.exit game function.c
+++ 23.exit game function.c
@@ -1,7 +1,8 @@
 void exitGame(FILE *fp) {
-    int exitz;
+    char exitz;
     printf("press E for exit the game\n");
     scanf(" %c", &exitz);
+    while (getchar() != '\n'); 
     if (exitz == 'E' || exitz == 'e') {
         printf("Exiting the game. Goodbye!\n");
         fprintf(fp, "\nGame exited by user.\n");
