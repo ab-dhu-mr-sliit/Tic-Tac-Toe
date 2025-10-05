0f1914f56d78dc4c681f02666e5408a7640a02f9
Abdur Rahman M.R <it25103195@my.sliit.lk>
2025-10-05 14:51:38 +0530

    Update 22.tryAgain function.c
    now, if you enter a character in the mode selection. it doesn't create infinite loop in the terminal

--- 22.tryAgain function.c
+++ 22.tryAgain function.c
@@ -1,12 +1,24 @@
-void tryAgain() {
-    printf("Press 0 play again or any other key to exit: ");
-    int playAgain;
-    scanf("%d", &playAgain);
-    if (playAgain == 0) {
-        system(CLEAR);
-        main(); // Restart the game
-    }
-    else {
-        printf("Exiting the game. Goodbye!\n");
-    }
+int tryMode(FILE *fp) {
+    int mode;
+    char input[100];
+    while (1) {
+        printf("Select Game Mode:\n\n");
+        printf("1. Two-Player (User vs User)\n");
+        printf("2. User vs Computer\n");
+        printf("3. Multi-Player (3 Players)\n\n");
+        printf("Enter your choice (1-3): ");
+        fgets(input, sizeof(input), stdin);
+        if (sscanf(input, "%d", &mode) == 1){
+            if (mode >= 1 && mode <= 3) {
+                logMode(fp, mode);
+                return mode;
+            } else {
+                printf("Invalid mode selected. Try again.\n\n");
+                logisValidMode(fp, mode);
+            } 
+        }
+        else {
+            printf("\nInvalid input. Please enter a number.\n\n");
+        }
+    }   
 }
