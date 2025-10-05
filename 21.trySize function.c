da7ce59d9bc5b16c3b08f364dcdecbd899420fe7
Abdur Rahman M.R <it25103195@my.sliit.lk>
2025-10-05 14:50:14 +0530

    Update 21.trySize function.c
    now. if you enter a character in the size selection. it doesn't create infinite loop in the terminal

--- 21.trySize function.c
+++ 21.trySize function.c
@@ -1,14 +1,21 @@
 int trySize(FILE *fp) {
     int size;
+    char input[100];
+
     while (1) {
-        printf("\nEnter grid size (3 to 10): ");
-        scanf("%d", &size);
-        logSize(fp, size);
-        if (size >= 3 && size <= 10) {
-            return size;
+        printf("Enter grid size (3 to 10): ");
+        fgets(input, sizeof(input), stdin);
+
+        if (sscanf(input, "%d", &size) == 1) {
+            if (size >= 3 && size <= 10) {
+                logSize(fp, size);
+                return size;
+            } else {
+                printf("Invalid grid size. Try again.\n\n");
+                logisValidSize(fp, size);
+            }
         } else {
-            printf("Invalid grid size. Try again.\n");
-            logisValidSize(fp, size);
+            printf("\nInvalid input. Please enter a number with in 3 to 10.\n\n");
         }
     }
 }
